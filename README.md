d008d5edc4ae79a911b08006468742ad5706da8b
Haroon Rasheed <haroon.rasheed@gigamon.com>
2021-11-15 20:22:23 -0800

    Removed mandatory tf_path argument

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # terraformpy
 Python wrapper for Terraform
 
-# Example usage
+# Getting started
+
+Initialise the terraformpy module. "tf_path" variable
 
 ```
 import terraformpy
-tf_path = "~/."
 tf_client = TerraformPy()
+```
 
-tf_client.init(tf_path)
+Below method is used for Terraform init action
 
-tf_client.apply(tf_path, var1=value1, var2=value2, ...)
+```
+tf_client.init()
+```
 
-tf_client.output(tf_path, out_type = "json")
+Use the below method for Terraform Apply with any number of variables.
 
-tf_client.destroy(tf_path, var1=value1, var2=value2, ...)
+```
+tf_client.apply(var1=value1, var2=value2, ...)
+```
+Use the below method to get the Terraform output based on output type from tfstate file.
+```
+tf_client.output(out_type = "json")
+```
+
+Use the below method for Terraform Destroy with any number of variables.
+```
+tf_client.destroy(var1=value1, var2=value2, ...)
 
 ```
--- terraformpy.py
+++ terraformpy.py
@@ -4,6 +4,7 @@
     This module helps in running Terraform commands using python class
 
 """
+import os
 import subprocess
 import logging
 
@@ -12,68 +13,74 @@ class TerraformPy():
         This class is the wrapper class for Terraform commands
 
     """
-    def __init__(self) -> None:
+    def __init__(self, path=None) -> None:
+        """Initialization method for terraformpy
+
+        Args:
+            path (string, optional): Path from where you need to run Terraform scripts.
+            Defaults to current working directory.
+        """
         super().__init__()
         # Create a custom logger
         self.logger = logging.getLogger(__name__)
+        if path is not None:
+            self.tf_path = path
+        else:
+            self.tf_path = os.getcwd()
 
-    def init(self, path):
-        """This method is used to run Terraform init command to download plugins
-
-        Args:
-            path (string): Path from where you need to run Terraform init
+    def init(self):
         """
-        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log", path=path)
+        This method is used to run Terraform init command to download plugins
 
-    def apply(self, path, **inputs):
+        """
+        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log")
+
+    def apply(self, **inputs):
         """This method is used to run Terraform Apply with variables
 
+        Returns:
+            string : Returns json format of the terraform output
+        """
+        args = " ".join({" -var "+str(k)+ "=" + "'" +str(v)+ "'" for k, v in inputs.items()})
+        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log")
+        self._subprocess_cmd("terraform apply -auto-approve {0} -no-color 2>&1 | \
+                                tee -a tr_out.log".format(args))
+        return subprocess.check_output("terraform output -json -no-color 2>&1 | \
+                                tee -a tr_out.log", cwd=self.tf_path, shell=True)
+
+    def destroy(self, **inputs):
+        """
+        This method is used to run Terraform Destroy with variables
+
+        """
+        args = " ".join({" -var "+str(k)+ "=" + "'" +str(v)+ "'" for k, v in inputs.items()})
+        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log")
+        self._subprocess_cmd("terraform destroy -auto-approve {0} -no-color 2>&1 | \
+                                tee -a tr_out.log".format(args))
+
+    def output(self, out_type="json"):
+        """This method is to get the Terraform output from tf state file
+
         Args:
-            path (string): Path from where you need to run Terraform Apply
+            out_type (str, optional): Terraform output type. Defaults to "json".
 
         Returns:
-            dict: Returns json of the terraform output
+            string : Returns json format of the terraform output
         """
-        args = " ".join({" -var "+str(k)+ "=" + "'" +str(v)+ "'" for k, v in inputs.items()})
-        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log", path=path)
-        self._subprocess_cmd("terraform apply -auto-approve {0} -no-color 2>&1 | \
-                                tee -a tr_out.log".format(args), path=path)
-        return subprocess.check_output("terraform output -json -no-color 2>&1 | \
-                                tee -a tr_out.log", cwd=path, shell=True)
+        return subprocess.check_output("terraform output -{0} -no-color 2>&1 | \
+                                tee -a tr_out.log".format(out_type), cwd=self.tf_path, shell=True)
 
-    def destroy(self, path, **inputs):
-        """This method is used to run Terraform Destroy with variables
-
-        Args:
-            path (string): Path from where you need to run Terraform Destroy
-        """
-        args = " ".join({" -var "+str(k)+ "=" + "'" +str(v)+ "'" for k, v in inputs.items()})
-        self._subprocess_cmd("terraform init -no-color 2>&1 | tee tr_out.log", path=path)
-        self._subprocess_cmd("terraform destroy -auto-approve {0} -no-color 2>&1 | \
-                                tee -a tr_out.log".format(args), path=path)
-
-    def output(self, path, out_type="json"):
-        """Get the terraform output from the tf state file
-
-        Args:
-            path (string): Path from where you need to run Terraform output
-        """
-        output = self._subprocess_cmd("terraform output -{0} -no-color 2>&1"
-                                      .format(out_type), path=path)
-        return output
-
-    def _subprocess_cmd(self, command, path):
+    def _subprocess_cmd(self, command):
         """Command processor to run Terraform commands
 
         Args:
             command (string): Complete Terraform command which supposed to be executed
-            path (string): Path from where you need to run Terraform scripts
 
         Raises:
             ValueError: Raises error when Terraform execution fails for any reason
         """
         self.logger.debug("Running terraform command ==> %s", command)
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=path, shell=True)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=self.tf_path, shell=True)
         proc_stdout, proc_stderr = process.communicate()
         if (process.returncode != 0) or ('Error' in proc_stdout.decode('utf-8')):
             raise ValueError("ERROR: Terraform execution failed :"
